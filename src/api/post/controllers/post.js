e1d185aa9a7499366b85c1383e4a4215a3cd25a9
1kakadu1 <1kakady1@gmail.com>
2023-04-15 12:57:32 +0300

    init strapi admin

---
+++ src/api/post/controllers/post.js
@@ -0,0 +1,25 @@
+'use strict';
+
+/**
+ * post controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::post.post', ({strapi})=>({
+    async findOne(ctx){
+        const {slug} = ctx.params;
+
+        const entity = await strapi.db.query('api::post.post').findOne({
+            where: {slug},
+            populate: ["banner", "SEO"]
+        });
+
+        delete entity.SEO.id;
+        delete entity.banner.id;
+        
+        const sanitizedEntity = await this.sanitizeOutput(entity);
+
+        return this.transformResponse(sanitizedEntity);
+    }
+}));
